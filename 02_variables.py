41812ead0c097077856092f3b4282bf25d390a17
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2021-06-07 17:37:58 +1200

    Create 02_variables.py

---
+++ 02_variables.py
@@ -0,0 +1,16 @@
+#code to learn about variables
+
+#variables can be a string
+greeting = "hello world"
+
+#they can also be numbers
+fav_num = 10
+print(fav_num * 2)
+
+num_string = "5"
+print(num_string * 5)
+
+#below if a constant (the name is in UPPERCASE)
+PI = 3.14
+
+num_2 = 2 #not 2_num
