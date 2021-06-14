605ccde26440909608386d2d94286134acd40a1e
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2021-06-14 20:48:22 +1200

    Update 06_coffee_order.py

--- 06_coffee_order.py
+++ 06_coffee_order.py
@@ -4,11 +4,14 @@ print("*** Coffee Order Demo")
 keep_going = ""
 while keep_going == "":
 
-    want_coffee = input("Do you want coffee? ")
+    # Ask user if they want coffee
+    want_coffee = input("Do you want coffee? ") .lower()
+
+    # If the answer is no, continue the loop
     if want_coffee != "yes":
         print("wrong answer, you always want coffee")
         continue
-
+    # Otherwise print good choice and break the loop
     else:
         print("Good choice")
         break
